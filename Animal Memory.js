3152573fcc2ff42f4c7787e52db2a0dd9b8024ba
Paul Schoepplenberg <paulschoepplenberg@Pauls-MacBook-Air.local>
2020-06-23 22:46:55 +0200

    Shuffling and eventlistener added
    - Added function to shuffle array of background images
    - Added function to add event listener to all cards by their class name

--- Animal Memory.js
+++ Animal Memory.js
@@ -1,5 +1,11 @@
+let animals = ['Cat.jpg', 'Deer.jpg', 'Dog.jpg', 'Elephant.jpg', 'Lion.jpg', 'Rabbit.jpg', 'RedPanda.jpg', 'Seal.jpg', 'Cat.jpg', 'Deer.jpg', 'Dog.jpg', 'Elephant.jpg', 'Lion.jpg', 'Rabbit.jpg', 'RedPanda.jpg', 'Seal.jpg']
+let shufCards;
+let cards;
+
 function startGame() {
     switchDivs();
+    shufCards = shuffle(animals);
+    addListener();
 }
 
 function switchDivs() {
@@ -9,12 +15,27 @@ function switchDivs() {
     game.style.display = "block";
 }
 
-let cards = document.getElementsByClassName("memory-card");
-console.log(cards.length);
-for (let i = 0; i < cards.length; i++) {
-    cards[i].addEventListener("click", displayCard);
+function addListener() {
+    cards = document.querySelectorAll(".memory-card");
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener("click", displayCard);
+    }
 }
 
 function displayCard() {
     console.log("test");
 }
+
+function shuffle(deck) {
+    for (let i = deck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+    return deck;
+}
+
+// function createObjects(key, value) {
+//     for (let i = 0; i < key.length; i++) {
+//         key[i].background = value[i];
+//     }
+// }
